50669682f9ae4630b911d0ebf161633e1273d39b
jmpizza <jespitiap@unal.edu.co>
2024-04-28 02:50:39 +0000

    Creación de estilos para button, combobox y entry

--- Inscripciones.py
+++ Inscripciones.py
@@ -21,85 +21,124 @@ class Inscripciones_2:
         self.lblNoInscripcion.configure(background="#f7f9fd",font="{Arial} 11 {bold}",
                                         justify="left",state="normal",
                                         takefocus=False,text='No.Inscripción')
+        
+        self.entryStyle = ttk.Style()
+        self.entryStyle.configure('TEntry', background='#000000',font=('Arial', 11, 'bold'))
+        self.entryStyle.map('TEntry',
+                            foreground=[('disabled', '#000000'), ('!disabled', '#000000')], 
+                            fieldbackground=[('disabled', '#ffffff'), ('!disabled', '#ffffff')])
+        
+        self.cmbStyle = ttk.Style()
+        self.cmbStyle.configure('TCombobox', background='#ffffff',font=('Arial', 11, 'bold'))
+        self.cmbStyle.map('TCombobox',
+                          foreground=[('disabled', '#000000'), ('!disabled', '#f7f9fd')], 
+                          fieldbackground=[('disabled', '#ffffff'), ('!disabled', '#ffffff')])
+        
          #Label No. Inscripción
         self.lblNoInscripcion.place(anchor="nw", x=680, y=20)
-        #Entry No. Inscripción
-        self.num_Inscripcion = ttk.Entry(self.frm_1, name="num_inscripcion")
-        self.num_Inscripcion.configure(justify="right")
+        
+        #Combobox No_Inscripción
+        self.num_Inscripcion = ttk.Combobox(self.frm_1, name="num_inscripcion")
         self.num_Inscripcion.place(anchor="nw", width=100, x=682, y=42)
         
         #Label Fecha
         self.lblFecha = ttk.Label(self.frm_1, name="lblfecha")
         self.lblFecha.configure(background="#f7f9fd", text='Fecha:')
         self.lblFecha.place(anchor="nw", x=630, y=80)
+        
         #Entry Fecha
         self.fecha = ttk.Entry(self.frm_1, name="fecha")
         self.fecha.configure(justify="center")
-        self.fecha.place(anchor="nw", width=90, x=680, y=80)
+        self.fecha.place(anchor="nw", width=100, x=680, y=80)
+        
         #Label Alumno
         self.lblIdAlumno = ttk.Label(self.frm_1, name="lblidalumno")
         self.lblIdAlumno.configure(background="#f7f9fd", text='Id Alumno:')
         self.lblIdAlumno.place(anchor="nw", x=20, y=80)
+        
         #Combobox Alumno
         self.cmbx_Id_Alumno = ttk.Combobox(self.frm_1, name="cmbx_id_alumno")
         self.cmbx_Id_Alumno.place(anchor="nw", width=112, x=100, y=80)
+        
         #Label Alumno
         self.lblNombres = ttk.Label(self.frm_1, name="lblnombres")
         self.lblNombres.configure(text='Nombre(s):')
         self.lblNombres.place(anchor="nw", x=20, y=130)
+        
         #Entry Alumno
         self.nombres = ttk.Entry(self.frm_1, name="nombres")
         self.nombres.place(anchor="nw", width=200, x=100, y=130)
+        
         #Label Apellidos
         self.lblApellidos = ttk.Label(self.frm_1, name="lblapellidos")
         self.lblApellidos.configure(text='Apellido(s):')
         self.lblApellidos.place(anchor="nw", x=400, y=130)
+        
         #Entry Apellidos
         self.apellidos = ttk.Entry(self.frm_1, name="apellidos")
         self.apellidos.place(anchor="nw", width=200, x=485, y=130)
+        
         #Label Curso
         self.lblIdCurso = ttk.Label(self.frm_1, name="lblidcurso")
         self.lblIdCurso.configure(background="#f7f9fd",state="normal",text='Id Curso:')
         self.lblIdCurso.place(anchor="nw", x=20, y=185)
+        
         #Entry Curso
         self.id_Curso = ttk.Entry(self.frm_1, name="id_curso")
         self.id_Curso.configure(justify="left", width=166)
         self.id_Curso.place(anchor="nw", width=166, x=100, y=185)
+        
         #Label Descripción del Curso
         self.lblDscCurso = ttk.Label(self.frm_1, name="lbldsccurso")
         self.lblDscCurso.configure(background="#f7f9fd",state="normal",text='Curso:')
         self.lblDscCurso.place(anchor="nw", x=275, y=185)
+        
         #Entry de Descripción del Curso 
         self.descripc_Curso = ttk.Entry(self.frm_1, name="descripc_curso")
         self.descripc_Curso.configure(justify="left", width=166)
         self.descripc_Curso.place(anchor="nw", width=300, x=325, y=185)
+        
         #Label Horario
         self.lblHorario = ttk.Label(self.frm_1, name="label3")
         self.lblHorario.configure(background="#f7f9fd",state="normal",text='Hora:')
         self.lblHorario.place(anchor="nw", x=635, y=185)
+        
         #Entry del Horario
         self.horario = ttk.Entry(self.frm_1, name="entry3")
         self.horario.configure(justify="left", width=166)
         self.horario.place(anchor="nw", width=100, x=680, y=185)
 
         ''' Botones  de la Aplicación'''
+        #Botón Style
+        self.btnStyle = ttk.Style()
+        self.btnStyle.configure('TButton', background='#f7f9fd',font=('Arial', 11, 'bold'), relief='raised',padding=5)
+        self.btnStyle.map('TButton',
+                            foreground=[('!disabled', '#0000ff'), ('active', '#00ffff'), ('disabled', '#f7f9fe')], 
+                            background=[('!disabled', '#ffffff'), ('active', '#00ffff'), ('disabled', '#000000')])
+        
+        #Boton Buscar
+        self.btnGuardar = ttk.Button(self.frm_1, name="btnbuscar")
+        self.btnGuardar.configure(text='Buscar')
+        self.btnGuardar.place(anchor="nw", x=150, y=260)
+        
         #Botón Guardar
         self.btnGuardar = ttk.Button(self.frm_1, name="btnguardar")
         self.btnGuardar.configure(text='Guardar')
-        self.btnGuardar.place(anchor="nw", x=200, y=260)
+        self.btnGuardar.place(anchor="nw", x=250, y=260)
         
         #Botón Editar
         self.btnEditar = ttk.Button(self.frm_1, name="btneditar")
         self.btnEditar.configure(text='Editar')
-        self.btnEditar.place(anchor="nw", x=300, y=260)
+        self.btnEditar.place(anchor="nw", x=350, y=260)
         #Botón Eliminar
         self.btnEliminar = ttk.Button(self.frm_1, name="btneliminar")
         self.btnEliminar.configure(text='Eliminar')
-        self.btnEliminar.place(anchor="nw", x=400, y=260)
+        self.btnEliminar.place(anchor="nw", x=450, y=260)
         #Botón Cancelar
         self.btnCancelar = ttk.Button(self.frm_1, name="btncancelar")
         self.btnCancelar.configure(text='Cancelar')
-        self.btnCancelar.place(anchor="nw", x=500, y=260)
+        self.btnCancelar.place(anchor="nw", x=550, y=260)
+        
         #Separador
         separator1 = ttk.Separator(self.frm_1)
         separator1.configure(orient="horizontal")
@@ -126,6 +165,9 @@ class Inscripciones_2:
         self.scroll_Y = ttk.Scrollbar(self.frm_1, name="scroll_y")
         self.scroll_Y.configure(orient="vertical")
         self.scroll_Y.place(anchor="s", height=275, width=12, x=790, y=582)
+        
+        self.apellidos.configure(state="disabled")
+        
         self.frm_1.pack(side="top")
         self.frm_1.pack_propagate(0)
 
